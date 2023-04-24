c371332bcd4915d49807419092f9c2fef70456b0
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-04-24 11:54:05 +0200

    Modules
    Modules geinstalleerd

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// Maak een route voor de index
-// dit plak je aan de basis url van de api, /categories
-app.get("/", (request, response) => {
-  let categoriesUrl = url + "/categories";
+import express from "express";
 
-  fetchJson(categoriesUrl).then((data) => {
-    response.render("index", data);
-  });
-});
+// basis url voor de api
+const url = "https://api.vinimini.fdnd.nl/api/v1";
 
-// dit plak je aan de basis url van de api, /producten
-app.get("/", async (request, response) => {
-  let productenUrl = url + "/producten";
+// Maak een nieuwe express app
+const app = express();
 
-  fetchJson(productenUrl).then((data) => {
-    response.render("index", data);
-  });
-});
+// Stel in hoe we express gebruiken
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Stel de public map in
+app.use(express.static("public"));
+
+// Stel afhandeling van formulieren inzx
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Stel het poortnummer in en start express
 app.set("port", process.env.PORT || 8000);
