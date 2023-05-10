7074de150e5e71d31678455eb8975e78fdabcb5a
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-05-10 13:30:12 +0200

    Producten gefixt
    denk ik

--- index.js
+++ index.js
@@ -1,78 +1,73 @@
+// Express uit de nodemodules map
+import { log } from "console";
 import express from "express";
+import { ppid } from "process";
 
-// basis url voor de api
-const url = "https://api.vinimini.fdnd.nl/api/v1";
+const url = "https://api.vinimini.fdnd.nl/api/v1/producten"; // URL naar Json data
+const url2 = "https://api.vinimini.fdnd.nl/api/v1";
 
-// Maak een nieuwe express app
+// Maak een nieuwe express app aan
 const app = express();
 
-// Stel in hoe we express gebruiken
+//  Stel in hoe we express gebruiken
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-// Stel de public map in
+// Gebruik maken van de "public" map
 app.use(express.static("public"));
 
-// Stel afhandeling van formulieren inzx
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 // Maak een route voor de index
-app.get("/", (request, response) => {
-  let categoriesUrl = url + "/categories";
 
-  fetchJson(categoriesUrl).then((data) => {
-    response.render("index", data);
+app.get("/", (request, response) => {
+  response.render("index");
+});
+
+// app.get("/proces", (request, response) => {
+//   let id = request.query.productId || "clerg7e1y032z0auq7ex5rid9";
+//   let detailUrl = url2 + "/product?id=" + id;
+
+//   fetchJson(detailUrl).then((data) => {
+//     response.render("proces", data);
+//   });
+// });
+
+app.get("/alleproducten", (request, response) => {
+  let productenUrl = url;
+  fetchJson(productenUrl).then((data) => {
+    response.render("alleproducten", data);
   });
 });
 
-//stel de views in
-app.set("view engine", "ejs");
-app.set("views", "./views");
-
-// Stel afhandeling van formulieren in
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//hier komen de routes (Hulp van krijn om zo beide endpoints te kunnen gebruiken)
-app.get("/", (request, response) => {
-  const baseUrl = "https://api.vinimini.fdnd.nl/api/v1";
-  const samId = "/notities?id=clemozv3c3eod0bunahh71sx7";
-  const notitiesUrl = `${baseUrl}${samId}`;
-  fetchJson(notitiesUrl).then((notitiesData) => {
-    let categoriesUrl = baseUrl + "/categories";
-    fetchJson(categoriesUrl).then((categoriesData) => {
-      response.render("index", {
-        categories: categoriesData.categories,
-        notities: notitiesData.notities,
-      });
-    });
+app.get("/detail", (request, response) => {
+  let id = request.query.detailId || "clerps05z09jm0aw3vccjq5un";
+  let detailUrl2 = url2 + "/product?id=" + id;
+  console.log(detailUrl2);
+  fetchJson(detailUrl2).then((data) => {
+    // console.log(data)
+    response.render("detail", data);
   });
 });
 
-// Dit is de data uit de notitie api // Met dit stuk code willen we de data roepen uit de notitie api
-app.post("/", function (req, res, next) {
-  const baseurl = "https://api.vinimini.fdnd.nl/api/v1/";
-  const url = `${baseurl}`;
+// Post note (notitie) to API
+app.post("/kalender", function (req, res, next) {
   req.body.afgerond = false;
-  req.body.persoonId = "clemozv3c3eod0bunahh71sx7";
+  //req.body.persoonId = 'clemozv3c3eod0bunahh71sx7'
   req.body.datum = req.body.datum + ":00Z";
   req.body.herinnering = [req.body.herinnering + ":00Z"];
-  console.log(req.body);
+  // console.log(req.body)
+  postJson(url2 + "/notities", req.body).then((data) => {
+    // console.log(JSON.stringify(data))
 
-  //Nieuwe notite kunnen maken
-  postJson(url + "/notities", req.body).then((data) => {
-    console.log(JSON.stringify(data));
     let newNotitie = { ...req.body };
-
     if (data.success) {
-      res.redirect("/");
-      // /(shlash) Verwijst naar de homepage
+      res.redirect("/kalender");
+      // TODO: squad meegeven, message meegeven
+      // TODO: Toast meegeven aan de homepagina
     } else {
       const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`;
-      const newdata = { error: errormessage, values: newData };
+      const newdata = { error: errormessage, values: newNotitie };
 
-      res.render("index", newdata);
+      res.render("kalender", newdata);
     }
   });
 });
@@ -83,47 +78,8 @@ app.listen(app.get("port"), function () {
   console.log(`Application started on http://localhost:${app.get("port")}`);
 });
 
-/**
- * Wraps the fetch api and returns the response body parsed through json
- * @param {*} url the api endpoint to address
- * @returns the json response from the api endpoint
- */
-async function fetchJson(url) {
-  return await fetch(url)
-    .then((response) => response.json())
-    .catch((error) => error);
-}
-
-app.get("/kalender", (request, response) => {
-  response.render("kalender");
-});
-
-app.get("/test", (request, response) => {
-  response.render("test");
-});
-
-app.get("/inloggen", (request, response) => {
-  response.render("inloggen");
-});
-
-app.get("/alleproducten", (request, response) => {
-  response.render("alleproducten");
-});
-
-app.get("/speciaalvoormini", (request, response) => {
-  response.render("speciaalvoormini");
-});
-
-app.get("/ingelogd", (request, response) => {
-  response.render("ingelogd");
-});
-
-export async function postJson(url, body) {
-  return await fetch(url, {
-    method: "post",
-    body: JSON.stringify(body),
-    headers: { "Content-Type": "application/json" },
-  })
+async function fetchJson(url, payload = {}) {
+  return await fetch(url, payload)
     .then((response) => response.json())
     .catch((error) => error);
 }
